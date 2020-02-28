39eef86ad105e2104c613516b490246b5958ad93
Max Tsygankov <iddqdpwn@gmail.com>
2020-02-28 19:20:24 +0300

    english language support added

---
+++ resources/js/javascript.js
@@ -0,0 +1,49 @@
+function changeLangToRus() {
+
+	document.getElementById("eng_about_text").style.display = 'none';
+	document.getElementById("rus_about_text").style.display = 'block';
+	document.getElementById("eng_about").style.display = 'none';
+	document.getElementById("rus_about").style.display = 'block';	
+	document.getElementById("eng_skills").style.display = 'none';
+	document.getElementById("rus_skills").style.display = 'block';		
+	document.getElementById("eng_skills_text").style.display = 'none';
+	document.getElementById("rus_skills_text").style.display = 'block';
+	document.getElementById("eng_edu").style.display = 'none';
+	document.getElementById("rus_edu").style.display = 'block';
+	document.getElementById("eng_edu_text").style.display = 'none';
+	document.getElementById("rus_edu_text").style.display = 'block';
+	document.getElementById("eng_lang").style.display = 'none';
+	document.getElementById("rus_lang").style.display = 'block';
+	document.getElementById("eng_lang_text").style.display = 'none';
+	document.getElementById("rus_lang_text").style.display = 'block';
+	document.getElementById("rus_name").style.display = 'block';
+	document.getElementById("eng_name").style.display = 'none';
+	document.getElementById("rus_links").style.display = 'block';
+	document.getElementById("eng_links").style.display = 'none';
+
+}
+
+function changeLangToEng() {
+
+	document.getElementById("eng_about_text").style.display = 'block';
+	document.getElementById("rus_about_text").style.display = 'none';
+	document.getElementById("eng_about").style.display = 'block';
+	document.getElementById("rus_about").style.display = 'none';
+	document.getElementById("eng_skills").style.display = 'block';
+	document.getElementById("rus_skills").style.display = 'none';
+	document.getElementById("eng_skills_text").style.display = 'block';
+	document.getElementById("rus_skills_text").style.display = 'none';
+	document.getElementById("eng_edu").style.display = 'block';
+	document.getElementById("rus_edu").style.display = 'none';
+	document.getElementById("eng_edu_text").style.display = 'block';
+	document.getElementById("rus_edu_text").style.display = 'none';
+	document.getElementById("eng_lang").style.display = 'block';
+	document.getElementById("rus_lang").style.display = 'none';
+	document.getElementById("eng_lang_text").style.display = 'block';
+	document.getElementById("rus_lang_text").style.display = 'none';
+	document.getElementById("rus_name").style.display = 'none';
+	document.getElementById("eng_name").style.display = 'block';
+	document.getElementById("eng_links").style.display = 'block';
+	document.getElementById("rus_links").style.display = 'none';
+	
+}
